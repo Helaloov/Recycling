9ea63e366eb1cdf812e7d333d5de97644393818f
Helal <helal.alsabbagh.syr@gmail.com>
2021-02-05 20:37:53 +0100

    new section

---
+++ app/src/main/java/com/example/pa/Abschnitt2_Kleidung.java
@@ -0,0 +1,14 @@
+package com.example.pa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Abschnitt2_Kleidung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abschnitt2__kleidung);
+    }
+}
