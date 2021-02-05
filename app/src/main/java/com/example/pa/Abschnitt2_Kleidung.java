a07b30a6e2baf8d232e56ab06e78fb3de641829e
Helal <helal.alsabbagh.syr@gmail.com>
2021-02-05 22:26:24 +0100

    Umfrage

--- app/src/main/java/com/example/pa/Abschnitt2_Kleidung.java
+++ app/src/main/java/com/example/pa/Abschnitt2_Kleidung.java
@@ -2,13 +2,30 @@ package com.example.pa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Abschnitt2_Kleidung extends AppCompatActivity {
 
+    Button Umfrage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abschnitt2__kleidung);
+
+        final Intent umfrage = new Intent(this, Umfrage.class);
+
+        Umfrage = (Button) findViewById(R.id.button6);
+
+        Umfrage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(umfrage);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/pa/Umfrage.java
@@ -0,0 +1,26 @@
+package com.example.pa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Umfrage extends AppCompatActivity {
+
+    WebView wr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_umfrage);
+
+        wr = findViewById(R.id.wr);
+
+        wr.setWebViewClient(new WebViewClient());
+        WebSettings tebSettings = wr.getSettings();
+        tebSettings.setJavaScriptEnabled(true);
+        wr.loadUrl("www.youtube.com");
+    }
+}
