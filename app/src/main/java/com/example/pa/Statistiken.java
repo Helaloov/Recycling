92334e213e3f7de43b44c424b8369433adb349ee
Helal <helal.alsabbagh.syr@gmail.com>
2021-03-06 20:32:29 +0100

    Klassen Pie- und Barchart wurden erstellt

---
+++ app/src/main/java/com/example/pa/Statistiken.java
@@ -0,0 +1,19 @@
+package com.example.pa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Statistiken extends AppCompatActivity {
+
+    Button b1 , b2 ,b3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistiken);
+
+
+
+    }
+}
