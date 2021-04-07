c60bdc1b6ddbf7a0d97f94f725eae8012eb6fdd0
Helal <helal.alsabbagh.syr@gmail.com>
2021-04-07 19:03:57 +0200

    häufige Fragen

--- app/src/main/java/com/example/pa/Abschnitt2_Kleidung.java
+++ app/src/main/java/com/example/pa/Abschnitt2_Kleidung.java
@@ -9,21 +9,32 @@ import android.widget.Button;
 
 public class Abschnitt2_Kleidung extends AppCompatActivity {
 
+    Button H_Fragen;
     Button Statistiken;
     Button Quiz;
     Button Umfrage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abschnitt2__kleidung);
 
+        final Intent h_Fragen = new Intent(this, HaufigeFragen.class);
         final Intent statistiken = new Intent(this, Statistiken.class);
         final Intent quiz = new Intent(this, Quiz.class);
         final Intent umfrage = new Intent(this, Umfrage.class);
 
+        H_Fragen = (Button) findViewById(R.id.button3);
         Statistiken = (Button) findViewById(R.id.button4);
         Quiz    = (Button) findViewById(R.id.button5);
         Umfrage = (Button) findViewById(R.id.button6);
+
+        H_Fragen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(h_Fragen);
+            }
+        });
 
         Statistiken.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/pa/HaufigeFragen.java
@@ -0,0 +1,28 @@
+package com.example.pa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class HaufigeFragen extends AppCompatActivity {
+
+    WebView wr ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_haufige_fragen);
+
+
+            wr = findViewById(R.id.wr);
+
+            wr.setWebViewClient(new WebViewClient());
+            WebSettings tebSettings = wr.getSettings();
+            tebSettings.setJavaScriptEnabled(true);
+            wr.loadUrl("file:///android_asset/f.html");
+    }
+}
